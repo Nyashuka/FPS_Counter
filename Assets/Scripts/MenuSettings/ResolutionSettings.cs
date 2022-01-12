29124b807609ab39ea5655369d93a8a91fad1062
OlegNevmitiy <godnebeles@gmail.com>
2022-01-12 03:08:17 +0200

    gfd

--- Assets/Scripts/MenuSettings/ResolutionSettings.cs
+++ Assets/Scripts/MenuSettings/ResolutionSettings.cs
@@ -8,25 +8,44 @@ public class ResolutionSettings : MonoBehaviour
 {
     [SerializeField] private Dropdown _dropdown;
 
+    private Resolution[] resolutions;
+
     private void Start()
     {
-        _dropdown.ClearOptions();
-        Resolution[] resolutions = Screen.resolutions;
-        resolutions.Distinct<Resolution>().ToArray();
+        _dropdown.ClearOptions();    
+
+        _dropdown.AddOptions(GetResolutionTitles().ToList());
+
+        _dropdown.value = GetCurrentResolution();
+    }
+
+    private string[] GetResolutionTitles()
+    {
+        resolutions = Screen.resolutions.Distinct().ToArray();
 
         string[] resolutionTitles = new string[resolutions.Length];
-        for(int i = 0; i < resolutions.Length; i++)
+        for (int i = 0; i < resolutions.Length; i++)
         {
-            resolutionTitles[i] = resolutions[i].ToString();
+            resolutionTitles[i] = resolutions[i].width + "x" + resolutions[i].height;
         }
-        
 
-        _dropdown.AddOptions(resolutionTitles.ToList());
+        return resolutionTitles;
+    }
 
+    private int GetCurrentResolution()
+    {
+        for (int i = 0; i < _dropdown.options.Count; i++)
+        {
+            string[] curRes = _dropdown.options[i].text.Split('x');
+            if (int.Parse(curRes[0]) == Screen.currentResolution.width && int.Parse(curRes[1]) == Screen.currentResolution.height)
+                return i;
+        }
+
+        return _dropdown.options.Count - 1;
     }
 
     public void ChangeResolution()
     {
-
+        Screen.SetResolution(resolutions[_dropdown.value].width, resolutions[_dropdown.value].height, true);
     }
 }
